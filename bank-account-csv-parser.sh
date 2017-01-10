df26a0b8b5e066cf61ba2ffb623dbea1d0c65df9
edwardgale <edward.gale@gmail.com>
2017-01-10 11:18:23 +0000

    Update bank-account-csv-parser.sh

--- bank-account-csv-parser.sh
+++ bank-account-csv-parser.sh
@@ -1,5 +1,6 @@
 #echo "Download csv file(s) from Lloyds online banking"
 #echo "Pass file(s) to the script"
 #echo "This assumes a USA locale set in File->Spreadsheet settings"
+#echo "Usage: ./bank-account-csv-parser.sh "myaccountdownload*.csv"
 
 cat $1 | grep -v "Transaction Description" | awk -F "," '{x=$1;split(x, a, "/") ;print a[2]"-"a[1]"-"a[3]","$5","$2","$7","$6","$8 }' | sort -n -t"-" -k3 -k1 -k2 -r
